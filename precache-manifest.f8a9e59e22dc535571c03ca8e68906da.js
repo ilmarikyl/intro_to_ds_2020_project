5c5e391de957595ba832749fcbe24fe4ab303c15
Olli Rousu <olli.rousu@hotmail.com>
2020-09-25 14:25:08 +0300

    Updates

---
+++ precache-manifest.f8a9e59e22dc535571c03ca8e68906da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ea14b0856a534b8cfa00b39ccb9017",
+    "url": "/intro_to_ds_2020_project/index.html"
+  },
+  {
+    "revision": "14215866760ebe4ea4f6",
+    "url": "/intro_to_ds_2020_project/static/css/2.32a52a2a.chunk.css"
+  },
+  {
+    "revision": "6330b0dc4a830586ed90",
+    "url": "/intro_to_ds_2020_project/static/css/main.07afabc0.chunk.css"
+  },
+  {
+    "revision": "14215866760ebe4ea4f6",
+    "url": "/intro_to_ds_2020_project/static/js/2.2a57474f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/intro_to_ds_2020_project/static/js/2.2a57474f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6330b0dc4a830586ed90",
+    "url": "/intro_to_ds_2020_project/static/js/main.c49cbb31.chunk.js"
+  },
+  {
+    "revision": "457646a5a653924fcb0d",
+    "url": "/intro_to_ds_2020_project/static/js/runtime-main.6b04a8e8.js"
+  }
+]);

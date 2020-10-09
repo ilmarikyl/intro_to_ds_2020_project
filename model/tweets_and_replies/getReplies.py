3631a3021885d1bba1ebcdfb9528f0ec332da580
tiila <tiila.kaenniemi@gmail.com>
2020-10-09 16:31:38 +0300

    changes to getreplies.py

--- model/tweets_and_replies/getReplies.py
+++ model/tweets_and_replies/getReplies.py
@@ -2,6 +2,7 @@ import tweepy
 import csv
 
 # Variables that contains the credentials to access Twitter API
+
 ACCESS_TOKEN = ''
 ACCESS_SECRET = ''
 CONSUMER_KEY = ''
@@ -17,35 +18,34 @@ def connect_to_twitter_OAuth():
 # Create API object
 api = connect_to_twitter_OAuth()
 
-#firstTweet = api.user_timeline("@MariaOhisalo")[2]
+# Get user
+user_name = 'MariaOhisalo'
+
+# Get tweet
+firstTweet = api.user_timeline("@MariaOhisalo")[3]
 #firstTweet = api.user_timeline("@JoeBiden")[0]
 
-#print(firstTweet.text)
-#id = firstTweet.id
-#print("Tweet ID", id)
-
-# Get user
-#user = api.get_user("JoeBiden")
-user_name = 'JoeBiden'
-
 # Set tweet id 
-#tweet_id = id
-tweet_id = '1313319274677768193' # biden 6.10.
+tweet_id = firstTweet.id
+#tweet_id = '1313319274677768193' # biden 6.10.
 
+replies = []
 
-# Create a list for all replies to a tweet
-replies=[]
-
-#koita muuttaa parametrejä tässä?
 with open("tweet_replies_most_pos_biden.csv", "w", newline="") as file:
     writer = csv.writer(file)
-    #while (len(replies) <= 100):
-    for tweet in tweepy.Cursor(api.search,q='to:'+user_name, result_type='recent', timeout=999999).items(1000): # original 1000
-        if hasattr(tweet, 'in_reply_to_status_id_str'):
-            if (tweet.in_reply_to_status_id_str==tweet_id):
-                replies.append(tweet)
-                reply_text = tweet.text
-                # Write reply to csv
-                writer.writerow([reply_text])
-                print(reply_text)
-
+    while (len(replies) <= 100):
+        for tweet in tweepy.Cursor(api.search,q='to:'+user_name, result_type='recent', tweet_mode="extended", timeout=999999).items(100): # original 1000
+            if hasattr(tweet, 'in_reply_to_status_id_str'):
+                if (tweet.in_reply_to_status_id_str==tweet_id):
+                    replies.append(tweet)
+                    # print reply text
+                    print(tweet.text)
+                    print(tweet.full_text)
+                    print(tweet.id)
+                    #status = api.get_status(reply_id, tweet_mode="extended")
+                    #print(status.full_text)
+                    #replies.append(tweet)
+                    #reply_text = tweet.text
+                    # Write reply to csv
+                    #writer.writerow([reply_text])
+                    #print(reply_text)
